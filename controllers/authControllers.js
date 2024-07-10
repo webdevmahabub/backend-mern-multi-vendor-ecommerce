c2e5d313ad2dcebadc3164d204279eeba9dfe1ba
webdevmahabub <mahbubbd926@gmail.com>
2024-07-10 21:01:29 +0600

    Seller Register Login

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,7 +1,9 @@
 const adminModel = require('../models/adminModel')
+const sellerModel = require('../models/sellerModel')
 const { responseReturn } = require('../utiles/response')
 const bcrpty = require('bcrypt')
 const { createToken } = require('../utiles/tokenCreate')
+const sellerCustomerModel = require('../models/chat/sellerCustomerModel')
 
 class authControllers{
     admin_login = async(req,res) => {
@@ -32,6 +34,69 @@ class authControllers{
         }
     }
     // End Method 
+
+    seller_login = async(req,res) => {
+        const {email,password} = req.body
+        try {
+            const seller = await sellerModel.findOne({email}).select('+password')
+            // console.log(admin)
+            if (seller) {
+                const match = await bcrpty.compare(password, seller.password)
+                // console.log(match)
+                if (match) {
+                    const token = await createToken({
+                        id : seller.id,
+                        role : seller.role
+                    })
+                    res.cookie('accessToken',token,{
+                        expires : new Date(Date.now() + 7*24*60*60*1000 )
+                    }) 
+                    responseReturn(res,200,{token,message: "Login Success"})
+                } else {
+                    responseReturn(res,404,{error: "Password Wrong"})
+                }
+
+
+            } else {
+                responseReturn(res,404,{error: "Email not Found"})
+            }
+
+        } catch (error) {
+            responseReturn(res,500,{error: error.message})
+        }
+
+    }
+    // End Method 
+
+    seller_register = async(req, res) => {
+        const {email,name,password} = req.body
+        try {
+            const getUser = await sellerModel.findOne({email})
+            if (getUser) {
+                responseReturn(res,404,{error: 'Email Already Exit'})
+            }else{
+                const seller = await sellerModel.create({
+                    name,
+                    email,
+                    password: await bcrpty.hash(password, 10),
+                    method : 'manually',
+                    shopInfo: {}
+                })
+                await sellerCustomerModel.create({
+                    myId: seller.id
+              })
+
+              const token = await createToken({ id : seller.id, role: seller.role })
+              res.cookie('accessToken',token, {
+               expires : new Date(Date.now() + 7*24*60*60*1000 )
+              })
+
+              responseReturn(res,201,{token,message: 'Register Success'})
+            }
+         } catch (error) {
+            responseReturn(res,500,{error: 'Internal Server Error'})
+         }
+    }
 
     getUser = async (req, res) => {
         const {id, role} = req;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,4 +4,7 @@ const { authMiddleware } = require('../middlewares/authMiddleware')
 
 router.post('/admin-login',authControllers.admin_login)
 router.get('/get-user',authMiddleware, authControllers.getUser)
+router.post('/seller-register',authControllers.seller_register)
+router.post('/seller-login',authControllers.seller_login)
+
 module.exports = router
