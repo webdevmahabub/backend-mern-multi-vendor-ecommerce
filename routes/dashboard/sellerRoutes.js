fd6dd8591c068eb2f190e2784f51d52ac2d379a8
webdevmahabub <mahbubbd926@gmail.com>
2024-10-15 14:44:10 +0600

    Seller Request Show Add part 2

---
+++ routes/dashboard/sellerRoutes.js
@@ -0,0 +1,7 @@
+const sellerController = require('../../controllers/dasboard/sellerController') 
+const { authMiddleware } = require('../../middlewares/authMiddleware')
+const router = require('express').Router()
+
+router.get('/request-seller-get',authMiddleware, sellerController.request_seller_get)  
+
+module.exports = router
