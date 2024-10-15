97c602261874fcb7262910ba261a94ad5a475a32
webdevmahabub <mahbubbd926@gmail.com>
2024-10-15 13:52:05 +0600

    Profile image upload complete

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -4,6 +4,8 @@ const { responseReturn } = require('../utiles/response')
 const bcrpty = require('bcrypt')
 const { createToken } = require('../utiles/tokenCreate')
 const sellerCustomerModel = require('../models/chat/sellerCustomerModel')
+const cloudinary = require('cloudinary').v2
+const formidable = require("formidable")
 
 class authControllers{
     admin_login = async(req,res) => {
@@ -97,6 +99,38 @@ class authControllers{
             responseReturn(res,500,{error: 'Internal Server Error'})
          }
     }
+
+    profile_image_upload = async(req, res) => {
+        const {id} = req
+        const form = formidable({ multiples: true })
+        form.parse(req, async(err,_,files) => {
+                cloudinary.config({
+                cloud_name: process.env.cloud_name,
+                api_key: process.env.api_key,
+                api_secret: process.env.api_secret,
+                secure: true
+            })
+            const { image } = files
+
+            try {
+                const result = await cloudinary.uploader.upload(image.filepath, { folder: 'profile'})
+                if (result) {
+                    await sellerModel.findByIdAndUpdate(id, {
+                        image: result.url
+                    }) 
+                    const userInfo = await sellerModel.findById(id)
+                    responseReturn(res, 201,{ message : 'Profile Image Upload Successfully',userInfo})
+                } else {
+                    responseReturn(res, 404,{ error : 'Image Upload Failed'})
+                }
+            } catch (error) {
+                responseReturn(res, 500,{ error : error.message })
+            }
+
+        })
+    }
+
+    // End Method 
 
     getUser = async (req, res) => {
         const {id, role} = req;
