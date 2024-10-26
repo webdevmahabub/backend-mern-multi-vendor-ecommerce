4484a4337f678e73ed37733d2d58ecf8f1569333
webdevmahabub <mahbubbd926@gmail.com>
2024-10-27 01:09:31 +0600

    Fetch Feature Product and Latest Top Rated Discount Part 1

--- routes/home/homeRoutes.js
+++ routes/home/homeRoutes.js
@@ -1,5 +1,6 @@
 const homeControllers = require('../../controllers/home/homeControllers') 
 const router = require('express').Router()
 router.get('/get-categorys',homeControllers.get_categorys)
- 
+router.get('/get-products',homeControllers.get_products)
+
 module.exports = router 
