1c99a4ae51525c7ba49bd5971bb1b77fca9b97d7
webdevmahabub <mahbubbd926@gmail.com>
2024-10-28 21:12:54 +0600

    Fetch Feature Product and Latest Top Rated Discount Part 2

--- controllers/home/homeControllers.js
+++ controllers/home/homeControllers.js
@@ -4,8 +4,22 @@ const productModel = require('../../models/productModel')
 class homeControllers{
     get_categorys = async(req,res) => {
 
-        formateProduct = () => {
-        
+        formateProduct = (products) => {
+            const productArray = [];
+            let i = 0
+            while (i < products.length) {
+                let temp = []
+                let j = i
+                while (j < i + 3) {
+                    if (products[j]) {
+                        temp.push(products[j])
+                    }
+                    j++
+                }
+                productArray.push([...temp])
+                i = j
+            }
+            return productArray
         }
 
         try {
