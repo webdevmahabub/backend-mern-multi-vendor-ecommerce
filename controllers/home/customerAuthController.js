0939037de424dc6cf2c915eba9fc6696667f0004
webdevmahabub <mahbubbd926@gmail.com>
2024-11-03 23:42:26 +0600

    Customer Register Setup Part 2

---
+++ controllers/home/customerAuthController.js
@@ -0,0 +1,6 @@
+class customerAuthController{
+    customer_register = async(req,res) => {
+        console.log(req.body)
+    }
+}
+module.exports = new customerAuthController()
---
+++ routes/home/customerAuthRoutes.js
@@ -0,0 +1,5 @@
+const customerAuthController = require('../../controllers/home/customerAuthController')
+const router = require('express').Router()
+router.post('/customer/customer-register',customerAuthController.customer_register)
+ 
+module.exports = router 
