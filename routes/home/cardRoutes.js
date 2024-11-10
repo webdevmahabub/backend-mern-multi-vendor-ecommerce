5b0fd29740070500f231c6c1c0ad25a249b6513c
webdevmahabub <mahbubbd926@gmail.com>
2024-11-10 21:50:51 +0600

    Product Add to Card Part 2

---
+++ routes/home/cardRoutes.js
@@ -0,0 +1,4 @@
+const cardController = require('../../controllers/home/cardController')
+const router = require('express').Router()
+router.post('/home/product/add-to-card',cardController.add_to_card) 
+module.exports = rout
