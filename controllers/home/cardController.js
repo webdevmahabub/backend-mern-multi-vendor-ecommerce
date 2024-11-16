883d7d0065a6c3684562bd9defb7f59b97b2dc21
webdevmahabub <mahbubbd926@gmail.com>
2024-11-16 14:07:16 +0600

    Wishlist Product Get and Delete Part 1

--- controllers/home/cardController.js
+++ controllers/home/cardController.js
@@ -188,5 +188,22 @@ class cardController{
         }
     }
     // End Method 
+
+    get_wishlist = async (req, res) => {
+        const { userId } = req.params
+        try {
+            const wishlists = await wishlistModel.find({
+                userId
+            })
+            responseReturn(res, 200, {
+                wishlistCount: wishlists.length,
+                wishlists
+            })
+            
+        } catch (error) {
+            console.log(error.message)
+        }
+       } 
+        // End Method 
 }
 module.exports = new cardController()
