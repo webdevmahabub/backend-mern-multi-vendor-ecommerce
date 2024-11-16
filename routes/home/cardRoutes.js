8b33f272333182582c2b4c42c586bc6168b3d1de
webdevmahabub <mahbubbd926@gmail.com>
2024-11-16 13:46:35 +0600

    Customer add Product in Wishlist Part

--- routes/home/cardRoutes.js
+++ routes/home/cardRoutes.js
@@ -5,4 +5,5 @@ router.get('/home/product/get-card-product/:userId',cardController.get_card_prod
 router.delete('/home/product/delete-card-product/:card_id',cardController.delete_card_products)
 router.put('/home/product/quantity-inc/:card_id',cardController.quantity_inc)
 router.put('/home/product/quantity-dec/:card_id',cardController.quantity_dec)
+router.post('/home/product/add-to-wishlist',cardController.add_wishlist) 
 module.exports = router
