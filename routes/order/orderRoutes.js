e0dafd651ff7379d158c9cf588c7c785e1bf1b2b
webdevmahabub <mahbubbd926@gmail.com>
2024-12-10 16:42:26 +0600

    Orders Get and Show Part 2

--- routes/order/orderRoutes.js
+++ routes/order/orderRoutes.js
@@ -1,9 +1,11 @@
 const orderController = require('../../controllers/order/orderController')
 const router = require('express').Router()
-
+// Customer
 router.post('/home/order/place-order',orderController.place_order) 
 router.get('/home/customer/get-dashboard-data/:userId',orderController.get_customer_dashboard_data)
 router.get('/home/customer/get-orders/:customerId/:status',orderController.get_orders)
 router.get('/home/customer/get-order-details/:orderId',orderController.get_order_details)
-
+// Admin
+router.get('/admin/orders',orderController.get_admin_orders)
+// Seller  
 module.exports = router 
