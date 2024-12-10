4846dee3dc0b7fc96f4fd15cb3cf13423ab8d724
webdevmahabub <mahbubbd926@gmail.com>
2024-12-10 16:16:47 +0600

    Sellers Get and Show Part 4

--- routes/dashboard/sellerRoutes.js
+++ routes/dashboard/sellerRoutes.js
@@ -6,4 +6,5 @@ router.get('/request-seller-get',authMiddleware, sellerController.request_seller
 router.get('/get-seller/:sellerId',authMiddleware, sellerController.get_seller)  
 router.post('/seller-status-update',authMiddleware, sellerController.seller_status_update)  
 router.get('/get-sellers',authMiddleware, sellerController.get_active_sellers) 
+router.get('/get-deactive-sellers',authMiddleware, sellerController.get_deactive_sellers) 
 module.exports = router
